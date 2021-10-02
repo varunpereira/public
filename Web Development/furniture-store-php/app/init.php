98f6cad31697ba39a4e289751af732273d0639d7
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2021-10-02 12:10:05 +1000

    Delete Web Development/furniture-store-php directory

--- Web Development/furniture-store-php/app/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "../app/core/app.php";
-require "../app/core/functions.php";
-require "../app/core/controller.php";
-require "../app/core/database.php";
-require "../app/core/config.php";
